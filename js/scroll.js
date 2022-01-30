a4d7f84f048d333b69f09c10bf4dfcbddfd840c0
sanghee01 <sang6299@naver.com>
2022-01-31 02:30:30 +0900

    JS: scrollTo -fixed
    Navbar menu click

--- js/scroll.js
+++ js/scroll.js
@@ -1,33 +1,13 @@
 "use strict";
-const navbarMenu = document.querySelectorAll(".navbar__menu__item");
-const homeLoc = document.querySelector("#home").offsetTop;
-const aboutLoc = document.querySelector("#about").offsetTop;
-const skillsLoc = document.querySelector("#skills").offsetTop;
-const workLoc = document.querySelector("#work").offsetTop;
-const testimonLoc = document.querySelector("#testimonials").offsetTop;
-const contactLoc = document.querySelector("#contact").offsetTop;
-const contactmeBtn = document.querySelector(".home__contact");
 
-navbarMenu[0].addEventListener("click", () => {
-  window.scrollTo({ top: homeLoc - navbarHeight, behavior: "smooth" });
-  console.dir(navbar);
-});
-navbarMenu[1].addEventListener("click", () => {
-  window.scrollTo({ top: aboutLoc - navbarHeight, behavior: "smooth" });
-  console.dir(navbar);
-});
-navbarMenu[2].addEventListener("click", () => {
-  window.scrollTo({ top: skillsLoc - navbarHeight, behavior: "smooth" });
-});
-navbarMenu[3].addEventListener("click", () => {
-  window.scrollTo({ top: workLoc - navbarHeight, behavior: "smooth" });
-});
-navbarMenu[4].addEventListener("click", () => {
-  window.scrollTo({ top: testimonLoc - navbarHeight, behavior: "smooth" });
-});
-navbarMenu[5].addEventListener("click", () => {
-  window.scrollTo({ top: contactLoc - navbarHeight, behavior: "smooth" });
-});
-contactmeBtn.addEventListener("click", () => {
-  window.scrollTo({ top: contactLoc - navbarHeight, behavior: "smooth" });
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
