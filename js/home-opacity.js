c8ff7d199b5e7d72da2268c77f3923415aa30899
sanghee01 <sang6299@naver.com>
2022-02-01 00:13:49 +0900

    JS: home transparent

---
+++ js/home-opacity.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
